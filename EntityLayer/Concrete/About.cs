0d72aad1a00c105d26f7e8d585c0c7623a40162c
Mehmet Ali Erdal <mehmetalierdal26@gmail.com>
2025-08-01 03:21:05 +0300

    Added Entities

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        [Key]
+        public int AboutID { get; set; }
+        public string FirstDetails { get; set; }
+        public string SecondDetails { get; set; }
+        public string FirstImage { get; set; }
+        public string SecondImage { get; set; }
+        public string MapLocation { get; set; }
+        public bool Status { get; set; }
+    }
+}
