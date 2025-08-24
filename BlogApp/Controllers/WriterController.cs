2a109ac011f06489c8228a40b24bb007c208b508
Mehmet Ali Erdal <mehmetalierdal26@gmail.com>
2025-08-24 19:33:03 +0300

    Lesson 51-55

--- BlogApp/Controllers/WriterController.cs
+++ BlogApp/Controllers/WriterController.cs
@@ -1,14 +1,65 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace BlogApp.Controllers
 {
     [AllowAnonymous]
     public class WriterController : Controller
     {
+        BlogManager bm = new BlogManager(new EFBlogRepository());
         public IActionResult Index()
         {
             return View();
         }
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult WriterHeaderNavbarPartial()
+        {
+            return PartialView();
+        }
+        public IActionResult MyBlogs()
+        {
+            var values = bm.TGetBlogListByWriter(1);
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult NewBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewBlog(Blog blog)
+        {
+            BlogValidator bv = new BlogValidator();
+            ValidationResult results = bv.Validate(blog);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+            else
+            {
+                blog.CreateDate = DateTime.Now;
+                blog.Status = true;
+                blog.WriterID = 1;
+                bm.TAdd(blog);
+                return RedirectToAction("MyBlogs");
+            }
+            
+        }
     }
+
+
 }
---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator : AbstractValidator<Blog>
+    {
+        public BlogValidator() 
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Blog title cannot be empty.");
+            RuleFor(x => x.Content).NotEmpty().WithMessage("Content cannot be empty.");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Blog image cannot be empty.");
+            RuleFor(x => x.Title).MaximumLength(150).WithMessage("Please enter a blog title with a maximum of 150 characters.");
+            RuleFor(x => x.Title).MinimumLength(5).WithMessage("Please enter a name with a minimum of 5 characters.");
+        }
+
+
+    }
+}
